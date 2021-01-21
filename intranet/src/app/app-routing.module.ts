06d9477a1fb106cb10ff828ec3eb2c4aacf70256
Florencia Alberto <flopy_2792@hotmail.com.ar>
2021-01-21 00:47:54 -0300

    nuevo logout

--- intranet/src/app/app-routing.module.ts
+++ intranet/src/app/app-routing.module.ts
@@ -11,10 +11,12 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'logout',
     loadChildren: () => import('./logout/logout.module').then( m => m.LogoutPageModule)
   },
+  
 
 
 ];
