f20ef5b251d76dc67506422ba1499555aeab37c8
Pamela Perez <perezxpamela@gmail.com>
2021-03-08 15:06:46 -0300

    Se agregó el contenido de la página herramientas corp.

--- intranet/src/app/herramientas/herramientas.page.ts
+++ intranet/src/app/herramientas/herramientas.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-herramientas',
   templateUrl: './herramientas.page.html',
   styleUrls: ['./herramientas.page.scss'],
+  
 })
 export class HerramientasPage implements OnInit {
-
-  constructor() { }
+  
+  constructor(private content: IonContent) { }
 
   ngOnInit() {
   }
-
+  gestionOffSet:any;
+  administrativasOffSet:any;
+  publicasOffSet:any;
+  ionViewDidEnter(){
+    //this.gestionOffSet=document.getElementById('h-gestion').offsetTop;
+    //this.administrativasOffSet=document.getElementById('h-administrativas').offsetTop;
+    //this.publicasOffSet=document.getElementById('h-publicas').offsetTop;
+  }
+  
+  /*
+  scrollToGestion() { 
+    this.content.scrollToPoint(0, this.gestionOffSet); 
+  }
+  scrollToAdministrativas() { 
+    this.content.scrollToPoint(0, this.administrativasOffSet); 
+  }
+  scrollToPublicas() { 
+    this.content.scrollToPoint(0, this.publicasOffSet); 
+  } */
+    
+      
+    
+    
+    
 }
+
