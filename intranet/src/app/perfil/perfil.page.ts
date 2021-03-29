db9c526d0c6d17970c3855d02ee4970236901937
Florencia Alberto <flopy_2792@hotmail.com.ar>
2021-03-29 12:23:47 -0300

    arreglado color perfil

--- intranet/src/app/perfil/perfil.page.ts
+++ intranet/src/app/perfil/perfil.page.ts
@@ -17,6 +17,7 @@ export class PerfilPage implements OnInit {
   colorElegido;
   contador:number=0;
 
+  color:string='background:linear-gradient(90deg,#53AF32 0%, #387823 100%);'
   
   colores: Array<{codigo: string}> = 
   [
@@ -201,6 +202,9 @@ cargar(){
 
 volverInicio(){
   this._router.navigate(['/home']);//vuelve a home
+  this.color='background:linear-gradient(90deg,#0075BE 0%, #004D7E 100%);'
+  this.usuarioDatosService.devolverColor(this.color)
+  this.usuarioDatosService.refreshMenu();
 }
 
 
